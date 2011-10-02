fb7ab319bccb29428c043a00dfd91a1748a5f990
Sylvain Utard - redox <sylvain.utard@gmail.com>
2011-10-02 19:49:40 +0200

    Display it on the right by default, as before

--- lib/last_projects_hooks.rb
+++ lib/last_projects_hooks.rb
@@ -1,6 +1,6 @@
 class LastProjectsHooks < Redmine::Hook::ViewListener
   
-    def view_welcome_index_left(context = {})
+    def view_welcome_index_right(context = {})
       context[:controller].render_to_string("/last_projects/home_box", :locals => { :projects => Project.latest(User.current) }, :layout => false)
     end
 
